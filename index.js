3dbd71b267e360d8784f5e7f574996ad662c468f
Ben Hawley <benhawley7@gmail.com>
2020-02-29 12:01:10 +0000

    feature: Module can call all non-beta endpoints

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * fortnite-api-io
+ * Module to make API calls to fortniteapi.io
+ *
+ * @author Ben Hawley
+ * @license MIT
+ */
+
+"use strict";
+
+/**
+ * index.js
+ * Export the API Class
+ */
+const FortniteAPI = require("./lib/fortnite-api");
+module.exports = FortniteAPI;
